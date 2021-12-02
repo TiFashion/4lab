fea08af3301a31abf5dfc1f54557701d8b4bd65d
TiFashion <86518014+TiFashion@users.noreply.github.com>
2021-12-02 20:22:42 +0300

    Добавьте файлы проекта.

---
+++ 4lab/4lab.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+int main() {
+    int a = 0;
+    int b = 0;
+    char operation;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "Enter operation: ";
+    cin >> operation;
+
+    switch (operation) {
+    case '-':
+        cout << "a - b = " << a - b << '\n';
+        break;
+    case '+':
+        cout << "a + b = " << a + b << '\n';
+        break;
+    case '*':
+        cout << "a * b = " << a * b << '\n';
+        break;
+    case '/':
+       cout << "a / b = " << a / b << '\n';
+        break;
+    default:
+        cout << "Error\n";
+    }
+    return 0;
+}
